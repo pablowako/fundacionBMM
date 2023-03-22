fb8a00a70c28d29c3f1caa6e85a6bbb0350cb24f
pablowako <pablowako@gmail.com>
2023-03-22 16:45:28 +0100

    Header changes, main CSS

--- fundacionBMM/src/App.jsx
+++ fundacionBMM/src/App.jsx
@@ -1,23 +1,20 @@
 import "./App.scss";
 import Header from "./components/Header/Header";
 import Home from "./pages/Home/Home";
-import {
-    BrowserRouter,
-    Route,
-    Routes,
-} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Book from "./pages/Book/Book";
-
 
 function App() {
     return (
         <div className="App">
             <BrowserRouter>
                 <Header />
-                <Routes>
-                    <Route exact path="/" element={<Home />} />
-                    <Route path="/book" element={<Book />} />
-                </Routes>
+                <div className="main">
+                    <Routes>
+                        <Route exact path="/" element={<Home />} />
+                        <Route path="/book" element={<Book />} />
+                    </Routes>
+                </div>
             </BrowserRouter>
         </div>
     );
